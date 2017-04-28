50c88ab9ea0fd8f60b23b8db6b3f1d25471f4a8a
monica-kuppuswamy <mpadmana@uncc.edu>
2017-04-28 00:07:23 -0400

    Latest changes in calculating distance vector for each router

--- src/DistanceVector.java
+++ src/DistanceVector.java
@@ -1,15 +1,10 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.nio.file.Files;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -20,75 +15,60 @@ public class DistanceVector {
 	}
 	
 	public static void main(String[] args) throws IOException {
+		
 		// TODO Auto-generated method stub
-		
-//		String filePath = (System.getProperty("user.dir")).toString() + "\\routerinfo\\" + args[0];
-//		File f = new File(filePath);
-//		BufferedReader br = new BufferedReader(new FileReader(f));
-//		String fileLine = br.readLine();
-//		System.out.println(fileLine);
-//		br.close();
-		
 		String fileName = args[1];
 		Boolean flag = true;
-		byte[] sendData = new byte[1024];
 		byte[] receiveData = new byte[1024];
+		byte[] sendData = new byte[1024];
 		
 		int portNumber = Integer.parseInt(args[0]);
 		InetAddress IPAddress = InetAddress.getByName("localhost");
 		
 		DatagramSocket clientSocket = new DatagramSocket(portNumber);
 		
-		Router self = new Router();
+		// Neighbor Router
+		Router neighborRouter = new Router();
 		
-		Router neighbour = new Router();
-		neighbour.setRouterName("notSet");
+		// Source Router
+		Router srcRouter = new Router();
 
-		while(true){
-			try{
-				System.out.println("In try block");
-				if (flag == true){
+		while (true) {
+			try {
+				if (flag == true) {
 					clientSocket.setSoTimeout(15000);
 					flag = false;
 				}
+				
 				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 				clientSocket.receive(receivePacket);
-				File f = new File((System.getProperty("user.dir")).toString()+ "\\routerinfo\\" + fileName + "_neighbour.txt");
-				FileOutputStream fos = new FileOutputStream(f);
-				//fos.write(receiveData);
-				fos.write(receivePacket.getData());
-				fos.close();
-				neighbour.getExistingDistanceVector(fileName+"_neighbour");
 				
-			}
-			catch(Exception e){
-//				String path = "";
-//				File initialFile = new File(path);
-//				FileInputStream targetStream = new FileInputStream(initialFile);
-//				for (int i=0;i<1024;i++)
-//			     {
-//			         sendData[i]=(byte)targetStream.read();
-//			     }
-				System.out.println("In catch block");
-				flag = true;
+				neighborRouter = (Router) Router.toObject(receiveData);
+				srcRouter.getExistingDistanceVector(fileName);
+				neighborRouter.getExistingDistanceVector(neighborRouter.getRouterName());
+				srcRouter.recomputeDistanceVector(neighborRouter);
+				srcRouter.addToFile();
 				
-				if (neighbour.getRouterName()!= "notSet"){
-					self.recomputeDistanceVector(neighbour);
+				List<String> nodeCosts = new ArrayList<String>(srcRouter.getNeighbourCost().keySet());
+				for (int i = 0; i < nodeCosts.size(); i++) {
+					System.out.println("Shortest Path " + srcRouter.getRouterName() + "-" + nodeCosts.get(i) + ":" + 
+								"Next Hop is " + srcRouter.getNeighbourNextHop().get(nodeCosts.get(i)) + " and the cost is" + 
+								srcRouter.getNeighbourCost().get(nodeCosts.get(i)));
 				}
-				
-				self.addToFile(self);
-				
-				sendData = Files.readAllBytes(new File((System.getProperty("user.dir")).toString()+ "\\routerinfo\\" + fileName + ".txt").toPath());
-
-				Iterator<Entry<String, Integer>> it = self.getNeighbourPort().entrySet().iterator();
+				System.out.println("\n");
+			}
+			
+			catch (Exception e) {
+				flag = true;
+				srcRouter.getExistingDistanceVector(fileName);
+				sendData = Router.toBytes(srcRouter);
+				Iterator<Entry<String, Integer>> it = srcRouter.getNeighbourPort().entrySet().iterator();
 			    while (it.hasNext()) {
 			        Map.Entry<String, Integer> pair = (Entry<String, Integer>)it.next();
-//			        System.out.println(pair.getKey() + " = " + pair.getValue());
 					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, pair.getValue());
 					clientSocket.send(sendPacket);
 			        it.remove();
 			    }
-				
 			}
 		}
 	}
--- src/Router.java
+++ src/Router.java
@@ -1,11 +1,17 @@
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.HashMap;
 
-public class Router {
+@SuppressWarnings("serial")
+public class Router implements Serializable {
 	
 	String routerName;
 	int noOfNeighbours;
@@ -80,31 +86,30 @@ public class Router {
 		String fileLine = null;
 		while((fileLine = br.readLine()) != null) {
 			String []parameters = fileLine.split(" ");
-			neighbourCost.put(parameters[0], Double.parseDouble(parameters[1]));
+			neighbourCost.put(parameters[0], Double.parseDouble(parameters[1].trim()));
 			setNeighbourCost(neighbourCost);
 			
 			neighbourNextHop.put(parameters[0], parameters[2]);
 			setNeighbourNextHop(neighbourNextHop);
-			try{
-				neighbourPort.put(parameters[0], Integer.parseInt(parameters[3]));
+			
+			if (parameters.length == 4) {	
+				neighbourPort.put(parameters[0], Integer.parseInt(parameters[3].trim()));
 				setNeighbourPort(neighbourPort);
-			}
-			catch(Exception e){
-				System.out.println(e);
 			}
 		}
 		br.close();
 	}
 	
-	public void addToFile(Router r) throws IOException {
-		String filePath = (System.getProperty("user.dir")).toString() + "\\routerinfo\\" + r.routerName + ".txt";
+	public void addToFile() throws IOException {
+		String filePath = (System.getProperty("user.dir")).toString() + "\\routerinfo\\" + this.routerName + ".txt";
 		FileWriter fw = new FileWriter(filePath, false);
-		fw.write(Integer.toString(r.noOfNeighbours));
-		for (String key : r.neighbourCost.keySet()) {
-			if (r.neighbourPort.get(key) != null) {
-				fw.write("\n" + key +  " " + r.neighbourCost.get(key) + " " + r.neighbourNextHop.get(key) + " " + r.neighbourPort.get(key));
+		fw.write(Integer.toString(this.noOfNeighbours));
+		for (String key : this.neighbourCost.keySet()) {
+			if (this.neighbourPort.get(key) != null) {
+				fw.write("\n" + key +  " " + this.neighbourCost.get(key) + " " + this.neighbourNextHop.get(key) + " " + 
+							this.neighbourPort.get(key));
 			} else {
-				fw.write("\n" + key +  " " + r.neighbourCost.get(key) + " " + r.neighbourNextHop.get(key));
+				fw.write("\n" + key +  " " + this.neighbourCost.get(key) + " " + this.neighbourNextHop.get(key));
 			}
 		}
 		fw.close();
@@ -112,24 +117,48 @@ public class Router {
 	
 	public void recomputeDistanceVector(Router advertisingRouter) throws IOException {
 		String nextHop = advertisingRouter.getRouterName();
-		double costFromRcvToAd = neighbourCost.get(advertisingRouter.getRouterName());
+		double costFromRcvToAd = this.neighbourCost.get(advertisingRouter.getRouterName());
 		for (String adNeighbour : advertisingRouter.neighbourCost.keySet()) {
-			if (neighbourCost.keySet().contains(adNeighbour) && !(adNeighbour.equals(routerName))) {
-				if (neighbourCost.get(adNeighbour) != null)
+			if (this.neighbourCost.keySet().contains(adNeighbour) && !(adNeighbour.equals(routerName))) {
+				if (this.neighbourCost.get(adNeighbour) != null)
 				{
-					if (advertisingRouter.neighbourCost.get(adNeighbour) + costFromRcvToAd < neighbourCost.get(adNeighbour)) {
-						neighbourCost.put(adNeighbour, advertisingRouter.neighbourCost.get(adNeighbour) + costFromRcvToAd);
-						neighbourNextHop.put(adNeighbour, nextHop);
+					if (advertisingRouter.neighbourCost.get(adNeighbour) + costFromRcvToAd < this.neighbourCost.get(adNeighbour)) {
+						this.neighbourCost.put(adNeighbour, advertisingRouter.neighbourCost.get(adNeighbour) + costFromRcvToAd);
+						this.neighbourNextHop.put(adNeighbour, nextHop);
 					}
 				}
 			} else {
 				if (!(adNeighbour.equals(routerName))) {
-					System.out.println(adNeighbour);
-					neighbourCost.put(adNeighbour, costFromRcvToAd + advertisingRouter.neighbourCost.get(adNeighbour));
-					neighbourNextHop.put(adNeighbour, nextHop);
+					this.neighbourCost.put(adNeighbour, costFromRcvToAd + advertisingRouter.neighbourCost.get(adNeighbour));
+					this.neighbourNextHop.put(adNeighbour, nextHop);
 				}
 			}
 		}
-		
-	}	
+	}
+	
+	/**
+	 * Converts an object to byte
+	 * @param o The object which has to converted to byte
+	 * @return The byte equivalent for the object passed
+	 * @throws IOException
+	 */
+	public static byte[] toBytes(Object o) throws IOException {
+		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+		ObjectOutputStream objStream = new ObjectOutputStream(byteStream);
+		objStream.writeObject(o);
+		return byteStream.toByteArray();
+	}
+	
+	/**
+	 * Converts a byte to Object
+	 * @param b The byte which has to converted to object
+	 * @return The converted data from a byte to object
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
+	public static Object toObject(byte[] b) throws ClassNotFoundException, IOException {
+		ByteArrayInputStream byteStream = new ByteArrayInputStream(b);
+		ObjectInputStream objStream = new ObjectInputStream(byteStream);
+		return objStream.readObject();
+	}
 }
